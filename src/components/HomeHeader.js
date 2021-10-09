a5374999b1077b80dbc22e3672474effb5743767
Dawid-Buchwald <79808900+Dawid-Buchwald@users.noreply.github.com>
2021-10-09 19:59:30 +0200

    niemal ukończony header

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,28 +6,40 @@ const decoration = require("../assets/Decoration.svg").default;
 const HomeHeader = () => {
     return (
         <>
-           <container className="headerContainer">
+           <container className="header">
                 <div>
                     <img className="headerImage" src={headerImg} alt="box with some stuff"></img>
                 </div>
                 <div>
-                    <ul>
-                        <li>Zaloguj</li>
-                        <li>Załóż konto</li>
-                    </ul>
-                </div>
-                <div></div>
-                <div className="headerBox">
-                    <h1>
-                        <span>Zacznij pomagać!</span><span>Oddaj Niechciane rzeczy w zaufane ręce.</span>
-                    </h1>
-                    <img className="headerDecoration" src={decoration}></img>
-                    <div className="headerButtonBox">
-                        <button className="headerButton">oddaj rzeczy</button>
-                        <button className="headerButton">zorganizuj zbiórkę</button>
+                    <div className="headerMenu">
+                        <div>
+                        <ul className="menuSmall">
+                            <li className="signIn">Zaloguj</li>
+                            <li className="signUp">Załóż konto</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul className="menuBig">
+                            <li className="headerMenuBigStart">Start</li>
+                            <li>O co chodzi?</li>
+                            <li>O nas</li>
+                            <li>Fundacje i organizacje</li>
+                            <li>Kontakt</li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div className="headerBox">
+                        <h1>
+                            <span>Zacznij pomagać!</span><span>Oddaj Niechciane rzeczy w zaufane ręce.</span>
+                        </h1>
+                        <img className="headerDecoration" src={decoration}></img>
+                        <div className="headerButtonBox">
+                            <button className="headerButton headerButtonText">oddaj rzeczy</button>
+                            <button className="headerButton">zorganizuj zbiórkę</button>
+                        </div>
                     </div>
                 </div>
-           </container>
+            </container>
         </>
     );
 };
