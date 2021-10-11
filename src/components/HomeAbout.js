501d580212a79db0d76a450ead480add58cca930
Dawid-Buchwald <79808900+Dawid-Buchwald@users.noreply.github.com>
2021-10-11 18:53:43 +0200

    dodanie sekcji o nas

---
+++ src/components/HomeAbout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const decoration = require("../assets/Decoration.svg").default;
+
+const HomeAbout = () => {
+    return (
+        <>
+          
+        </>
+    );
+};
+
+export default HomeAbout;
